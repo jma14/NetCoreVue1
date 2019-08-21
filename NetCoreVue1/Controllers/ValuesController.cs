0d455e790f3a99751322deff8b560311a9dded6e
Jason <jarnoff14@comcast.net>
2019-08-20 22:45:01 -0400

    setting up projects/db connection

---
+++ NetCoreVue1/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    private IRepositoryWrapper _repoWrapper;
+
+    public ValuesController(IRepositoryWrapper repoWrapper)
+    {
+        _repoWrapper = repoWrapper;
+    }
+    // GET api/values
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        var domesticAccounts = _repoWrapper.Account.FindByCondition(x => x.AccountType.Equals("Domestic"));
+        var owners = _repoWrapper.Owner.FindAll();
+
+        return new string[] { "value1", "value2" };
+    }
+}
